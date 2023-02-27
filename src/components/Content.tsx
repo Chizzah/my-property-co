980cd085b11ad5a185540fe08b44b5750513b0e1
Chadd Poggenpoel <chadd@cubicit.co.za>
2023-02-27 07:42:05 +0200

    0.0.5

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,11 +1,7 @@
-import type { Component, JSXElement } from 'solid-js';
+import type { Component, JSXElement } from "solid-js";
 
 const Content: Component<{ children: JSXElement }> = ({ children }) => {
-  return (
-    <main class='flex-1'>
-      {children}
-    </main>
-  )
+  return <main class="flex-1 min-h-screen">{children}</main>;
 };
 
 export default Content;
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -6,7 +6,7 @@ import Header from "../components/Header";
 
 const MainLayout: Component<{ children: JSXElement }> = ({ children }) => {
   return (
-    <div class="relative flex flex-col w-full lg:max-w-[1440px] lg:pl-[27px] min-h-screen font-open-sans">
+    <div class="relative flex flex-col w-full lg:max-w-[1440px] lg:pl-[27px] font-open-sans">
       <Header />
       <Content>{children}</Content>
       <Footer />
